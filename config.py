4ea4e66454f49749f8efbf241910ffa90c637662
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-25 16:36:16 -0400

    flask config with with placeholder info for db_password

---
+++ config.py
@@ -0,0 +1,31 @@
+"""
+Flask configuration class for production,
+development, and testing
+"""
+
+class Config(object):
+
+    DEBUG = False
+    TESTING = False
+
+    DB_NAME = "production_db"
+    DB_PASSWORD ="this will change later"
+    UPLOADS = "./app/static/uploads"
+    SESSION_COOKIE_SECURE = True
+
+class Production(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DB_NAME = "development_db"
+    DB_PASSWORD = "change_this_later"
+    UPLOADS = "/flask_test/uploads"
+    SESSION_COOKIE_SECURE = False
+
+class TestingConfig(config):
+    TESTING = True
+    DB_NAME = "development_db"
+    DB_PASSWORD = "change_this_later"
+    UPLOADS = "/flask_test/uploads"
+    SESSION_COOKIE_SECURE = False
